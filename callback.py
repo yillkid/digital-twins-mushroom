8c819d0fc3a82a99e0dde4122252a9c4e3ea3b76
yillkid <yillkid@gmail.com>
2024-03-15 16:20:25 +0800

    feat: set daemon

--- callback.py
+++ callback.py
@@ -1,6 +1,27 @@
+"""
+- 適宜溫度: 25
+- 適宜濕度: 80
+- 滴灌每次噴霧，濕度增加 10%
+"""
+
 def set_device(google_sheet, env_param):
     sheet_ai = google_sheet.worksheet("AI")
-    sheet_ai.update("C2", "1")
-    sheet_ai.update("D2", "1")
+    
+    # 濕度 - 滴灌
+    if (int(env_param["humidity"]) < 80):
+        value = int((80 - int(env_param["humidity"]))/10)
+        env_param["drip"] = str(value)
+        sheet_ai.update("C2", str(value))
+    else:
+        env_param["drip"] = "0"
+        sheet_ai.update("C2", "0")
+
+    # 溫度 - 風扇
+    if (int(env_param["temperature"]) > 25):
+        env_param["fan"] = "1"
+        sheet_ai.update("D2", "1")
+    else:
+        env_param["fan"] = "0"
+        sheet_ai.update("D2", "0")
 
     return True
--- server.py
+++ server.py
@@ -2,10 +2,12 @@ import os
 import json
 import gspread
 import pytz
+import time
 from oauth2client.service_account import ServiceAccountCredentials
 from dotenv import load_dotenv
 from get_env import get_environment_params
 from callback import set_device
+from datetime import datetime
 
 load_dotenv()
 
@@ -14,9 +16,13 @@ scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/au
 creds = ServiceAccountCredentials.from_json_keyfile_name('secret/key.json', scope)
 client = gspread.authorize(creds)
 
-env_param = {"temperature":"","humidity":"", "drip":"", "fan":""}
-google_sheet = client.open_by_url(os.getenv("GOOGLE_SHEET_URL"))
-get_environment_params(google_sheet, env_param)
-set_device(google_sheet, env_param)
+while True:
+    env_param = {"temperature":"","humidity":"", "drip":"", "fan":""}
+    google_sheet = client.open_by_url(os.getenv("GOOGLE_SHEET_URL"))
+    get_environment_params(google_sheet, env_param)
+    set_device(google_sheet, env_param)
+    now = datetime.now()
+    formatted_now = now.strftime('%Y-%m-%d %H:%M:%S')
 
-print("hello env_param = " + json.dumps(env_param))
+    print(formatted_now + " : "  + json.dumps(env_param))
+    time.sleep(5)
